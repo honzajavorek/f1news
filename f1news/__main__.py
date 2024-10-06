24044849fc7f33b171e581784441d87627ae2210
Honza Javorek <mail@honzajavorek.cz>
2024-10-06 16:20:42 +0200

    changes, yolo

--- f1news/__main__.py
+++ f1news/__main__.py
@@ -12,13 +12,17 @@ from crawlee.beautifulsoup_crawler import (
 )
 from crawlee.configuration import Configuration
 from crawlee.router import Router
+from crawlee._log_config import CrawleeLogFormatter
 import httpx
 from lxml import etree
 from apify import Actor
 
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("f1")
+handler = logging.StreamHandler()
+handler.setFormatter(CrawleeLogFormatter())
+logger = logging.getLogger("f1news")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 
 router = Router[BeautifulSoupCrawlingContext]()
@@ -41,8 +45,8 @@ router = Router[BeautifulSoupCrawlingContext]()
 @click.option("--debug/--no-debug", default=False, help="Enable debug mode")
 def main(feed_url: str, output_path: Path, debug: bool):
     logger.setLevel(logging.DEBUG if debug else logging.INFO)
-    rss, url_mapping = asyncio.run(scrape(feed_url, debug=debug))
 
+    rss, url_mapping = asyncio.run(scrape(feed_url, debug=debug))
     xml = etree.fromstring(rss)
     namespaces = {'atom': 'http://www.w3.org/2005/Atom'}
     for entry in xml.xpath("//atom:entry", namespaces=namespaces):
@@ -74,13 +78,18 @@ async def scrape(feed_url: str, debug: bool = False):
             logger.warning("Couldn't infer proxy configuration")
             proxy_configuration = None
 
-        max_concurrency = 1 if Actor.is_at_home() else 200
-        logger.warning(f"Max concurrency: {max_concurrency}")
+        max_concurrency = 1 if Actor.is_at_home() else 5
+        logger.info(f"Max concurrency: {max_concurrency}")
+
+        max_request_retries = 10 if Actor.is_at_home() else 3
+        logger.info(f"Max request retries: {max_request_retries}")
 
         crawler = BeautifulSoupCrawler(
             request_handler=router,
             proxy_configuration=proxy_configuration,
             concurrency_settings=ConcurrencySettings(max_concurrency=max_concurrency),
+            max_request_retries=max_request_retries,
+            additional_http_error_status_codes=[403, 429],
             configuration=Configuration(log_level="DEBUG" if debug else "INFO"),
         )
         await crawler.run(links)
