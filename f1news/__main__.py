8cfdc74272f33d8f75892355869fad99c8774c74
Honza Javorek <mail@honzajavorek.cz>
2024-11-08 18:39:47 +0100

    use API

--- f1news/__main__.py
+++ f1news/__main__.py
@@ -1,11 +1,10 @@
 from pathlib import Path
-from urllib.parse import parse_qs
-from bs4 import BeautifulSoup
 import click
 import feedparser
 
 import httpx
 from lxml import etree
+import praw
 
 
 @click.command()
@@ -23,45 +22,51 @@ from lxml import etree
     type=click.Path(path_type=Path, dir_okay=False),
     help="Output file path",
 )
-def main(feed_url: str, output_path: Path):
+@click.option(
+    "--user-agent", default="F1news (+https://github.com/honzajavorek/f1news/)"
+)
+@click.option("--client-id", envvar="REDDIT_CLIENT_ID", required=True)
+@click.option("--client-secret", envvar="REDDIT_CLIENT_SECRET", required=True)
+def main(
+    feed_url: str,
+    output_path: Path,
+    user_agent: str,
+    client_id: str,
+    client_secret: str,
+):
+    click.echo("Initializing file system")
     output_path.parent.mkdir(parents=True, exist_ok=True)
-    with httpx.Client(
+
+    click.echo("Initializing Reddit API client")
+    reddit = praw.Reddit(
+        client_id=client_id, client_secret=client_secret, user_agent=user_agent
+    )
+    reddit.read_only = True
+
+    click.echo("Fetching feed")
+    response = httpx.get(
+        feed_url,
         follow_redirects=True,
         headers={
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:131.0) Gecko/20100101 Firefox/131.0",
+            "User-Agent": user_agent,
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.8,cs;q=0.6,sk;q=0.4,es;q=0.2",
-            "DNT": "1",
         },
-    ) as client:
-        click.echo("Fetching feed")
-        response = client.get(feed_url)
-        response.raise_for_status()
+    )
+    response.raise_for_status()
 
-        click.echo("Parsing feed")
-        xml_bytes = response.content
-        feed = feedparser.parse(xml_bytes)
+    click.echo("Parsing feed")
+    xml_bytes = response.content
+    feed = feedparser.parse(xml_bytes)
 
-        url_mapping = {}
-        for entry in feed.entries:
-            click.echo(f"Fetching: {entry.link}")
-            response = client.get(entry.link)
-            response.raise_for_status()
-
-            click.echo(f"Parsing: {entry.link}")
-            soup = BeautifulSoup(response.content, "html.parser")
-            flair_link = soup.select_one('a[href*="/r/formula1/?f=flair_name"]')
-            _, query_string = flair_link["href"].split("?")
-            f_param_value = parse_qs(query_string)["f"][0]
-            if f_param_value != 'flair_name:":post-news: News"':
-                click.echo(f"Not news: {entry.link}")
-                continue
-            article_link = soup.select_one(
-                'a[aria-label][target="_blank"][rel*="nofollow"][rel*="noopener"]'
-            )
-            article_url = article_link["href"]
-            click.echo(f"Recording: {entry.link}")
-            url_mapping[entry.link] = article_url
+    url_mapping = {}
+    for entry in feed.entries:
+        click.echo(f"Fetching: {entry.link}")
+        submission = reddit.submission(url=entry.link)
+        if submission.link_flair_text == ":post-news: News":
+            click.echo(f"Recording as {submission.url}")
+            url_mapping[entry.link] = submission.url
+        else:
+            click.echo("Not news")
 
     click.echo("Rewriting feed")
     xml = etree.fromstring(xml_bytes)
